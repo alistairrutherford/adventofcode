90842917ea31a0d2bcb8522c0431a7f916eaf119
alistairrutherford <alistair.rutherford@gmail.com>
2017-12-13 13:21:04 +0000

    WIP

--- bin/advent10b.dart
+++ bin/advent10b.dart
@@ -2,11 +2,15 @@
  * what is the result of multiplying the first two numbers in the list?
  *
  */
+import 'dart:convert';
 
-var INPUT = [147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70];
+var INOUT_STR = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70";
 
+List<int> TAIL_SEQ = [17, 31, 73, 47, 23];
 
 const SIZE = 256;
+
+const MAX_ROUND = 64;
 
 class HashArray {
   List<int> data = new List();
@@ -47,27 +51,35 @@ main(List<String> arguments) {
   int current = 0;
   int skip = 0;
 
-  for (int value in INPUT) {
+  // Build input
+  List<int> encoded = ASCII.encode(INOUT_STR);
 
-    int count = value ~/ 2;
+  List<int> values = new List();
+  values.addAll(encoded);
+  values.addAll(TAIL_SEQ);
 
-    for (int i = 0; i < count; i++) {
+  for (int round = 0; round < MAX_ROUND; round++) {
+    for (int value in values) {
+      int count = value ~/ 2;
 
-      int start = current + i;
-      int end = current + value - 1 - i;
+      for (int i = 0; i < count; i++) {
+        int start = current + i;
+        int end = current + value - 1 - i;
 
-      int first = hashArray.get(start);
-      int last = hashArray.get(end);
+        int first = hashArray.get(start);
+        int last = hashArray.get(end);
 
-      hashArray.put(start, last);
-      hashArray.put(end, first);
-    }
+        hashArray.put(start, last);
+        hashArray.put(end, first);
+      }
 
-    current += value + skip++;
-    if (current > SIZE) {
-      current = current % SIZE;
+      current += value + skip++;
+      if (current > SIZE) {
+        current = current % SIZE;
+      }
     }
   }
+
 
   int first = hashArray.get(0);
   int second = hashArray.get(1);
