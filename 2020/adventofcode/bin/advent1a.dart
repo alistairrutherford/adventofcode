3456b55aacdc4ecfd5e0bfe16d143283ebe8735f
alistairrutherford <alistairrutherford@gmail.com>
2020-12-02 19:36:50 +0000

    Completed advent 1

--- 2020/adventofcode/bin/advent1a.dart
+++ 2020/adventofcode/bin/advent1a.dart
@@ -6,26 +6,60 @@
 import 'dart:io';
 import 'dart:convert';
 
-  const String INPUT_DATA = "advent1a.txt";
+const String INPUT_DATA = "advent1a.txt";
+const int TARGET_VALUE = 2020;
 
 /**
  * Load input lines.
  */
-List<String> loadInput(String fileName) {
+List<int> loadInput(String fileName) {
   Directory current = Directory.current;
 
   String path = current.toString().split(' ')[1].replaceAll('\'','') + "/bin/" + fileName;
 
-  var file = new File(path);
+  var file = File(path);
 
   List<String> lines = file.readAsLinesSync(encoding: ascii);
 
-  return lines;
+  List<int> listOfInts = List<int>();
+  for (String value in lines) {
+    listOfInts.add(int.parse(value));
+  }
+
+  // Lowest to highest
+  listOfInts.sort();
+
+  return listOfInts;
 }
 
 main(List<String> arguments) {
 
-  List<String> inputs = loadInput(INPUT_DATA);
+  List<int> inputs = loadInput(INPUT_DATA);
 
-  print("Lines read: ${inputs.length}");
+  bool found = false;
+  int length = inputs.length;
+
+  int outerValue = 0;
+  int innerValue = 0;
+
+  int outer = 0;
+  while ((outer < length) && !found) {
+    outerValue = inputs[outer];
+    int inner = outer + 1;
+    while (inner < length && !found) {
+      innerValue = inputs[inner];
+
+      found = (innerValue + outerValue == TARGET_VALUE);
+
+      if (!found) inner++;
+    }
+
+    if (!found) outer++;
+  }
+
+  if (found) {
+    print("Values: ${innerValue}, ${outerValue}");
+
+    print("Answer: ${innerValue * outerValue}");
+  }
 }
