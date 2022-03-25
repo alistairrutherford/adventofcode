a42d054c561e84b05b9e3cfccac33380b858839f
alistairrutherford <alistair.rutherford@gmail.com>
2022-03-25 15:14:47 +0000

    Solution to advent10a.

--- 2021/adventofcode/bin/advent10a.dart
+++ 2021/adventofcode/bin/advent10a.dart
@@ -2,10 +2,13 @@
  * AdventOfCode 10
  */
 
+import 'dart:collection';
 import 'dart:io';
 import 'dart:convert';
 
-const String INPUT_FILE = "advent9.txt";
+const String INPUT_FILE = "advent10.txt";
+
+final SCORE_MAP = const {')': 3, ']': 57, '}': 1197, '>': 25137};
 
 /**
  * Load input and build data stricture
@@ -21,6 +24,55 @@ List<String> loadInput(String fileName) {
   return lines;
 }
 
+///
+/// Track opening and closing brackets
+///
+int updateCountMap(String char, Map<String, int> map) {
+  int scoreBracket1 = map['(']!;
+  int scoreBracket2 = map['[']!;
+  int scoreBracket3 = map['{']!;
+  int scoreBracket4 = map['<']!;
+
+  switch (char) {
+    case '(':
+      scoreBracket1++;
+      break;
+    case ')':
+      scoreBracket1--;
+      break;
+    case '[':
+      scoreBracket2++;
+      break;
+    case ']':
+      scoreBracket2--;
+      break;
+    case '{':
+      scoreBracket3++;
+      break;
+    case '}':
+      scoreBracket3--;
+      break;
+    case '<':
+      scoreBracket4++;
+      break;
+    case '>':
+      scoreBracket4--;
+      break;
+  }
+  map['('] = scoreBracket1;
+  map['['] = scoreBracket2;
+  map['{'] = scoreBracket3;
+  map['<'] = scoreBracket4;
+
+  // Return total count
+  int total = 0;
+  for (int count in map.values) {
+    total += count;
+  }
+  return total;
+}
+
+
   /**
    * Main.
    */
@@ -30,5 +82,26 @@ List<String> loadInput(String fileName) {
 
     int total = 0;
 
+
+    // Initialise map
+    Map<String, int> counts =  HashMap<String, int>();
+    counts['('] = 0;
+    counts['['] = 0;
+    counts['{'] = 0;
+    counts['<'] = 0;
+
+    // Rule: If the total map count is > number of remaining charavters in the line then it's incomplete
+    List<int> scores = List<int>.empty(growable: true);
+    for (String line in lines) {
+      int index = 0;
+      bool isCorrupt = false;
+      while (index < line.length && !isCorrupt) {
+        String char = line[index++];
+        int totalCount = updateCountMap(char, counts);
+        if (totalCount > (line.length - index)) {
+          isCorrupt = true;
+        }
+      }
+    }
     print("total: $total");
   }
